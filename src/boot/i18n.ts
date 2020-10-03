cc66ebc2decdbae9e11123edfa5ce6bfa457f695
Joel Watson <joelwat@gmail.com>
2020-10-02 18:44:50 -0700

    Updates boot files to Quasar latest

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,20 +1,17 @@
+import { boot } from 'quasar/wrappers';
+import messages from 'src/i18n';
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 
-import { BootConfig } from '@/types';
-import messages from 'src/i18n';
-
 Vue.use(VueI18n);
 
-const i18n = new VueI18n({
+export const i18n = new VueI18n({
   locale: 'en-us',
   fallbackLocale: 'en-us',
   messages,
 });
 
-export default ({ app }: { app: BootConfig }) => {
+export default boot(({ app }) => {
   // Set i18n instance on app
   app.i18n = i18n;
-};
-
-export { i18n };
+});
